0423a3c1cc8e0b1aabb5947aa389d782bfdf7d91
Madeleine Huish <madeleinehuish@gmail.com>
2019-06-07 05:41:30 -0700

    saves

---
+++ src/components/UserCreate.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Field from './Field';
+import Button from './Button';
+
+const UserCreate = () => {
+  return (
+    <Field />
+    <Button />
+  )
+}
+
+export default UserCreate;
